a935c2b17dbe00f7d29c5a160ab5ade08a321e4c
Arshle <329756669@qq.com>
2018-04-06 14:01:36 +0800

    添加服务

--- cloud-start/src/main/java/com/chezhibao/cloudstart/CloudStartApplication.java
+++ cloud-start/src/main/java/com/chezhibao/cloudstart/CloudStartApplication.java
@@ -2,7 +2,6 @@ package com.chezhibao.cloudstart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
