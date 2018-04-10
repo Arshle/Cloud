92f2c2e76c135d8eb0731e9789833146d499da12
Arshle <329756669@qq.com>
2018-04-11 00:35:10 +0800

    添加服务

---
+++ config-client/src/main/java/com/chezhibao/config/client/TestController.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C), 2018-2020,中间件开发工程师
+ * FileName: TestController.java
+ * Author:   Arshle
+ * Date:     2018年04月11日
+ * Description:
+ */
+package com.chezhibao.config.client;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 〈〉<br>
+ * 〈〉
+ *
+ * @author Arshle
+ * @see [相关类/方法]（可选）
+ * @since [产品/模块版本]（可选）
+ */
+@RefreshScope
+@RestController
+public class TestController {
+
+    @Value("${from}")
+    private String from;
+
+    @RequestMapping("/from")
+    public String from(){
+        return this.from;
+    }
+}
